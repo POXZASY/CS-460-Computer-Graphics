812a0cb4a3b5d7c0d01aa0e43a459518bd0c8f5c
Larkin Wisdom <lwisdom1@binghamton.edu>
2019-03-02 18:47:56 -0500

    fsdfds

--- Wisdom_CS460_HW2/CS_460_Project_2/CS_460_Project_2/CS_460_Project_2.cpp
+++ Wisdom_CS460_HW2/CS_460_Project_2/CS_460_Project_2/CS_460_Project_2.cpp
@@ -91,69 +91,30 @@ bool contains(vector<tuple<int, int>> vec, tuple<int,int> pt) {
 	return false;
 }
 
-//Scanline Fill Algorithm
-vector<tuple<int, int>> scanLineFill(vector<tuple<int, int>> polygonPoints) {
-	vector<tuple<int, int>> retVal;
-	int maxy = INT_MIN;
-	int miny = INT_MAX;
-	//find all points intersected by polygon
-	vector<tuple<int, int>> intersectPoints;
-	for (int i = 0; i < polygonPoints.size(); i++) { //iterate through each of the sides of the polygon
-		tuple<int, int> p1 = polygonPoints[i]; //first point
-		tuple<int, int> p2 = polygonPoints[(i + 1) % polygonPoints.size()]; //second point
-		int p1x = get<0>(p1);
-		int p1y = get<1>(p1);
-		int p2x = get<0>(p2);
-		int p2y = get<1>(p2);
-		//store max/min y for later
-		if (p1y > maxy) maxy = p1y;
-		if (p2y > maxy) maxy = p2y;
-		if (p1y < miny) miny = p1y;
-		if (p2y < miny) miny = p2y;
-		//slope of the line
-	//if slope is infinite
-		if (p2x - p1x == 0) {
-			for (int j = min(p1y, p2y); j <= max(p1y, p2y); j++) {
-				intersectPoints.push_back(make_tuple(p1x, j));
-			}
-		}
-		//normal slope
-		else {
-			float m = (float)(p2y - p1y) / (p2x - p1x);
-			//iterate through all of the x values between the two points
-			for (int j = min(p1x, p2x); j <= max(p1x, p2x); j++) {
-				intersectPoints.push_back(make_tuple(j, round(m*(j - p1x) + p1y)));
-			}
-		}
-	}
-	cout << "---------------------------------------" << endl;
-	cout << "Boundary Points: " << endl;
-	for (tuple<int, int> t : intersectPoints) {
-		cout << get<0>(t) << ", " << get<1>(t) << endl;
-	}
-	cout << "---------------------------------------" << endl;
-	//apply algorithm
-	for (int y = miny; y <= maxy; y++) { //iterate through the y values
-		int parity = 0;
-		bool lastpointnotboundary = true;
-		for (int x = 0; x <= screenx; x++) {
-			if (contains(intersectPoints, make_tuple(x, y)) && lastpointnotboundary) { //if we found a boundary point, and the previous point wasn't a boundary point
-				parity = (parity + 1) % 2;
-				lastpointnotboundary = false;
-			}
-			if (parity == 1 && !contains(intersectPoints, make_tuple(x, y))) { //if we are filling and the point is not a boundary
-				retVal.push_back(make_tuple(x, y));
-				lastpointnotboundary = true;
-			}
-			else if (parity == 0 && !contains(intersectPoints, make_tuple(x, y))) {
-				lastpointnotboundary = true;
-			}
-		}
-	}
-	cout << "Total Points: " << retVal.size() << endl;
-	return retVal;
+//Boundary Fill Algorithm
+struct Color {
+	float r;
+	float g;
+	float b;
+};
+Color getPixelColor(int x, int y) {
+	Color color;
+
 }
 
+void boundaryFill4(int x, int y, int boundary_color)
+{
+	if (getpixel(x, y) != boundary_color && getpixel(x, y) != fill_color){
+		//putpixel(x, y, fill_color);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex2f(x, y);
+		boundaryFill4(x + 1, y, boundary_color);
+		boundaryFill4(x, y + 1, boundary_color);
+		boundaryFill4(x - 1, y, boundary_color);
+		boundaryFill4(x, y - 1, boundary_color);
+	}
+	if()
+}
 void processMenu(int option) {
 	switch (option) {
 	
@@ -279,7 +240,7 @@ int main(int argc, char** argv) {
 	glutInit(&argc, argv); //Initialize glut
 	glutCreateWindow("Project 2");
 	glutInitWindowSize(screenx, screeny);
-	glutInitWindowPosition(50, 50);
+	glutInitWindowPosition(100, 100);
 	glutDisplayFunc(display);
 	glutMouseFunc(mouseHandler);
 	glutKeyboardFunc(keyBoard);
