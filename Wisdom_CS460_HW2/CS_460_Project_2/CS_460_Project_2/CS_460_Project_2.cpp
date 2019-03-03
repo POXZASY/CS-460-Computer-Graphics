08a7e9bdbaad0c1fc194be16a6bcefd034e5cb67
Larkin Wisdom <lwisdom1@binghamton.edu>
2019-03-03 01:12:49 -0500

    fdsf

--- Wisdom_CS460_HW2/CS_460_Project_2/CS_460_Project_2/CS_460_Project_2.cpp
+++ Wisdom_CS460_HW2/CS_460_Project_2/CS_460_Project_2/CS_460_Project_2.cpp
@@ -14,7 +14,7 @@ int menux;
 int menuy;
 float red = 0.0;
 float green = 1.0;
-float blue = 0.5;
+float blue = 0.0;
 vector<tuple<int, int>> polygon;
 vector<tuple<float, float>> polygonPointsOpenGL;
 bool currentlyFilling = false;
@@ -24,11 +24,17 @@ bool currentlyDrawing = false;
 bool closePoly = false;
 
 tuple<float, float> glutToGLCoords(int x, int y) {
-	float GLx = ((float)x) / (screenx / 2) - 1;
-	float GLy = -1 * (((float)y) / (screeny / 2) - 1);
+	float GLx = ((float)x) / ((float)screenx / 2) - 1;
+	float GLy = -1 * (((float)y) / ((float)screeny / 2) - 1);
 	return make_tuple(GLx, GLy);
 }
 
+tuple<int, int> GLtoGlutCoords(float x, float y) {
+	int glutX = (x + 1)*(screenx / 2);
+	int glutY = (-1 * y + 1)*(screeny / 2);
+	return make_tuple(glutX, glutY);
+
+}
 //returns x-intercept between two lines, uses glut coords
 int xintercept(int x_1, int y_1, int x_2, int y_2, int x_3, int y_3, int x_4, int y_4) {
 	int numerator = (x_1 * y_2 - y_1 * x_2) * (x_3 - x_4) - (x_1 - x_2) * (x_3 * y_4 - y_3 * x_4);
@@ -93,12 +99,14 @@ bool contains(vector<tuple<int, int>> vec, tuple<int,int> pt) {
 	return false;
 }
 
+
 //Boundary Fill Algorithm
+/*
 struct Color {
 	public:
-		float r;
-		float g;
-		float b;
+		GLfloat r;
+		GLfloat g;
+		GLfloat b;
 };
 bool sameColor(Color lhs, Color rhs) {
 	return (lhs.r == rhs.r && lhs.b == rhs.b && lhs.g == rhs.g);
@@ -106,16 +114,29 @@ bool sameColor(Color lhs, Color rhs) {
 Color getPixelColor(int x, int y) {
 	Color color;
 	glReadPixels(x, y, 1, 1, GL_RGB, GL_FLOAT, &color);
+	//cout << x << ", " << y << endl;
+	//cout << color.r << ", " << color.g << ", " << color.b << endl;
 	return color;
 }
 void boundaryFill4(int x, int y){
-	if (0 <= x <= screenx && 0 <= y <= screeny) { //if point is on the screen
-		Color black;
-		black.r = 0.0; black.g = 0.0; black.b = 0.0;
+	if (0 <= x && x <= screenx && 0 <= y && y <= screeny) { //if point is on the screen
+		Color blue;
+		blue.r = 0.0; blue.g = 0.0; blue.b = 1.0;
 		Color red;
 		red.r = 1.0; red.g = 0.0; red.b = 0.0;
-		if (!(sameColor(getPixelColor(x, y), black) && !(sameColor(getPixelColor(x, y), red)))) {
-			fillPoints.push_back(make_tuple(x, y));
+		
+		//cout << sameColor(getPixelColor(x, y), black) << endl;
+		if (!(sameColor(getPixelColor(x, y), blue) && !(sameColor(getPixelColor(x, y), red)))) {
+			cout << x << ", " << y << endl;
+			cout << getPixelColor(x,y).r << ", " << getPixelColor(x, y).g << ", " << getPixelColor(x, y).b << endl;
+			glBegin(GL_POINTS);
+			glColor3f(1.0, 0.0, 0.0);
+			float xval = get<0>(glutToGLCoords(x, y));
+			float yval = get<1>(glutToGLCoords(x, y));
+			glVertex2f(xval, yval);
+			glEnd();
+			cout << getPixelColor(x, y).r << ", " << getPixelColor(x, y).g << ", " << getPixelColor(x, y).b << endl;
+			//fillPoints.push_back(make_tuple(x, y));
 			boundaryFill4(x + 1, y);
 			boundaryFill4(x, y + 1);
 			boundaryFill4(x - 1, y);
@@ -123,6 +144,66 @@ void boundaryFill4(int x, int y){
 		}
 	}
 }
+*/
+
+//SE
+/*
+void draw_pixel(int x, int y)
+{
+	glColor3f(1.0, 0.0, 1.0);
+	glPointSize(1.0);
+	glBegin(GL_POINTS);
+	glVertex2i(x, y);
+	glEnd();
+}
+
+void edgedetect(float x1, float y1, float x2, float y2, int *le, int *re)
+{
+	float temp, x, mx;
+	int i;
+
+	if (y1 > y2)
+	{
+		temp = x1, x1 = x2, x2 = temp;
+		temp = y1, y1 = y2, y2 = temp;
+	}
+
+	if (y1 == y2)
+		mx = x2 - x1;
+	else
+		mx = (x2 - x1) / (y2 - y1);
+
+	x = x1;
+
+	for (i = int(y1);i <= (int)y2;i++)
+	{
+		if (x < (float)le[i]) le[i] = (int)x;
+		if (x > (float)re[i]) re[i] = (int)x;
+		x += mx;
+	}
+}
+
+void scanfill(vector<tuple<int, int>> polygonPoints){
+	vector<tuple<float, float>> floatPoints;
+	for (tuple<int, int> t : polygonPoints) {
+		floatPoints.push_back(make_tuple((float)get<0>(t),(float)get<1>(t)));
+	}
+	int le[500], re[500], i, j;
+
+	for (i = 0;i < 500;i++) le[i] = 500, re[i] = 0;
+
+	for (int k = 0; i < polygonPoints.size(); k++) {
+		edgedetect(get<0>(polygonPoints[i]), get<1>(polygonPoints[i]), get<0>(polygonPoints[(i + 1) % polygonPoints.size()]), get<1>(polygonPoints[(i + 1) % polygonPoints.size()]), le, re);
+	}
+
+	for (j = 0;j < 500;j++)	{
+		if (le[j] <= re[j])
+			for (i = le[j];i < re[j];i++)
+				draw_pixel(i, j);
+	}
+}
+*/
+
 void processMenu(int option) {
 	switch (option) {
 	
@@ -132,7 +213,8 @@ void processMenu(int option) {
 		break;
 	//Region Filling
 	case 2:
-		boundaryFill4(menux, menuy);
+		scanfill(polygon);
+		//boundaryFill4(menux, menuy);
 		break;
 	//Window-to-Viewport Mapping
 	case 3:
@@ -159,10 +241,6 @@ void mouseHandler(int button, int state, int x, int y) {
 		}
 		currentlyDrawing = true;
 		polygon.push_back(make_tuple(x, y));
-	}
-	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-		menux = x;
-		menuy = y;
 	}
 }
 
@@ -203,6 +281,7 @@ void display() {
 	}
 	glEnd();
 
+	/*
 	//filling the polygon
 	if (currentlyFilling) {
 		glBegin(GL_POINTS);
@@ -214,10 +293,11 @@ void display() {
 		}
 		glEnd();
 	}
-	glColor3f(0.0, 0.0, 0.0);
+	*/
+	glColor3f(0.0, 0.0, 1.0);
 
 	//dashed lines
-	glLineStipple(1, 0xFF00);
+	glLineStipple(1, 0xFFFF);
 	glEnable(GL_LINE_STIPPLE);
 	//draw square
 	glBegin(GL_LINES);
@@ -233,14 +313,19 @@ void display() {
 	//glPopAttrib();
 	glDisable(GL_LINE_STIPPLE);
 
-
 	glutReshapeWindow(500, 500);
 	glFlush();
 	glutPostRedisplay();
 }
+void menuFunc(int status, int x, int y) {
+	if (GLUT_MENU_IN_USE) {
+		menux = x;
+		menuy = y;
+	}
+}
 
 void mousePos(int x, int y) {
-	cout << "Red: " << getPixelColor(x, y).r << " Green: " << getPixelColor(x, y).g << " Blue: "<< getPixelColor(x, y).b << endl;
+	//cout << "Red: " << getPixelColor(x, y).r << " Green: " << getPixelColor(x, y).g << " Blue: "<< getPixelColor(x, y).b << endl;
 }
 int main(int argc, char** argv) {
 	glutInit(&argc, argv); //Initialize glut
@@ -251,6 +336,7 @@ int main(int argc, char** argv) {
 	glutMouseFunc(mouseHandler);
 	glutKeyboardFunc(keyBoard);
 	glutPassiveMotionFunc(mousePos);
+	glutMenuStatusFunc(menuFunc);
 	createMenu();
 	glutMainLoop();
 	return 0;
