c54f1097a41268243855cf1737b980f287b8e0a0
Larkin Wisdom <lwisdom1@binghamton.edu>
2019-04-01 00:32:39 -0400

    sdfsd

--- Wisdom_CS460_HW4/CS_460_Project_4/CS_460_Project_4/CS_460_Project_4.cpp
+++ Wisdom_CS460_HW4/CS_460_Project_4/CS_460_Project_4/CS_460_Project_4.cpp
@@ -3,6 +3,7 @@
 #include "GL/glut.h"
 #include <vector>
 #include <fstream>
+#include <math.h>
 
 using namespace std;
 
@@ -12,6 +13,15 @@ int screeny = 500;
 float red = 0.0;
 float green = 0.0;
 float blue = 0.0;
+
+bool start = true;
+
+bool mouseCurrentlyDown = false;
+bool modifiedImage = false;
+int initialMouseX;
+int initialMouseY;
+int changeX;
+int changeY;
 
 const int width = 384;
 const int height = 256;
@@ -98,12 +108,67 @@ void bitsToStructs() {
 	}
 }
 
-void structsToBits() {
-	int inc = 0;
+void morphImage() {
+	vector<vector<Color>> structimageq1_new(width, vector<Color>(height));
+	vector<vector<Color>> structimageq2_new(width, vector<Color>(height));
+	vector<vector<Color>> structimageq3_new(width, vector<Color>(height));
+	vector<vector<Color>> structimageq4_new(width, vector<Color>(height));
+
+	//Quadrant 1
+	float xratio1 = (float)(width / 2 - changeX) / (width / 2);
+	float yratio1 = (float)(height / 2 - changeY) / (height / 2);
+	//Quadrant 2
+	float xratio2 = (float)(width / 2 + changeX) / (width / 2);
+	float yratio2 = (float)(height / 2 - changeY) / (height / 2);
+	//Quadrant 3
+	float xratio3 = (float)(width / 2 + changeX) / (width / 2);
+	float yratio3 = (float)(height / 2 + changeY) / (height / 2);
+	//Quadrant 4
+	float xratio4 = (float)(width / 2 - changeX) / (width / 2);
+	float yratio4 = (float)(height / 2 + changeY) / (height / 2);
+
+	//Initial Point Translation
 	for (int j = 0; j < height; j++) {
 		for (int i = 0; i < width; i++) {
 			if (i < width / 2) { //left half
 				if (j < height / 2) {//3rd quadrant
+					cout << round(i*xratio3) << ", " << round(j*yratio3) << endl;
+					structimageq3_new[round(i*xratio3)][round(j*yratio3)] = structimageq3[i][j];
+				}
+				else { //2nd quadrant
+					structimageq2_new[round(i*xratio2)][round((j-height/2)*yratio2 + height/2)] = structimageq2[i][j];
+				}
+			}
+			else { //right half
+				if (j < height / 2) {//4th quadrant
+					structimageq4_new[round(i*xratio4)][round(j*yratio4)] = structimageq4[i][j];
+				}
+				else { //1st quadrant
+					structimageq1_new[round((i - width/2)*xratio1 + width/2)][round(j*yratio1)] = structimageq1[i][j];
+				}
+			}
+		}
+	}
+
+
+	//INTERPOLATION
+
+
+
+
+	//structimageq1 = structimageq1_new;
+	//structimageq2 = structimageq2_new;
+	structimageq3 = structimageq3_new;
+	//structimageq4 = structimageq4_new;
+
+}
+
+void structsToBits() {
+	int inc = 0;
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
+			if (i < width / 2 + changeX) { //left half
+				if (j < height / 2 + changeY) {//3rd quadrant
 					image[inc] = structimageq3[i][j].b;
 					inc++;
 					image[inc] = structimageq3[i][j].g;
@@ -121,7 +186,7 @@ void structsToBits() {
 				}
 			}
 			else { //right half
-				if (j < height / 2) {//4th quadrant
+				if (j < height / 2 + changeY) {//4th quadrant
 					image[inc] = structimageq4[i][j].b;
 					inc++;
 					image[inc] = structimageq4[i][j].g;
@@ -142,10 +207,7 @@ void structsToBits() {
 	}
 }
 
-
-
 void display() {
-	
 	glClearColor(red, green, blue, 0);
 	glColor3f(1.0, 1.0, 1.0);
 	glBegin(GL_QUADS);
@@ -154,13 +216,14 @@ void display() {
 	glVertex3f(.5, .5, 0);
 	glVertex3f(.5, -.5, 0);
 	glEnd();
-	
+
 	//populate "image" array
 	readBMP("flower.bmp");
 	bitsToStructs();
+	if(modifiedImage) morphImage();
 	structsToBits();
 	
-	glRasterPos2f(-1, -1);
+	glRasterPos2f(-.75, -.75);
 	glDrawPixels(width, height, GL_BGR_EXT, GL_UNSIGNED_BYTE, image);
 	//glRasterPos2f(-1, .1);
 	//glDrawPixels(width/2, height/2, GL_BGR_EXT, GL_UNSIGNED_BYTE, imageq1);
@@ -178,7 +241,24 @@ void display() {
 
 void mouseHandler(int button, int state, int x, int y) {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		if (!mouseCurrentlyDown) {
+			cout << "here" << endl;
+			initialMouseX = x;
+			initialMouseY = y;
+			mouseCurrentlyDown = true;
+			modifiedImage = true;
+		}
+	}
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+		mouseCurrentlyDown = false;
+	}
+}
 
+void mousePos(int x, int y) {
+	if (mouseCurrentlyDown) {
+		changeX = x - initialMouseX;
+		changeY = -(y - initialMouseY);
+		//cout << "X: " << changeX << ", Y: " << changeY << endl;
 	}
 }
 
@@ -187,9 +267,10 @@ int main(int argc, char** argv){
 	glutInitWindowSize(screenx, screeny);
 	glutInitWindowPosition(50, 50);
 	//glutInitDisplayMode(GLUT_DOUBLE);
-	glutCreateWindow("Project 3");
+	glutCreateWindow("Project 4");
 	glutDisplayFunc(display);
 	glutMouseFunc(mouseHandler);
+	glutMotionFunc(mousePos);
 	//glutKeyboardFunc(keyboardHandler);
 	glutMainLoop();
 	return 0;
